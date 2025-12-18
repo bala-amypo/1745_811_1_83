7f68472382a847e51a391b8deacd39f53b409c13
bala-amypo <bala@amypo.in>
2025-12-18 08:27:01 +0000

    User commit

---
+++ src/main/java/com/example/demo/entity/UserEntity.java
@@ -0,0 +1,49 @@
+package com.example.demo.entity;
+
+public class UserEntity {
+    Long id;
+    String email;
+    String password;
+    String role;
+
+}
+public UserEntity(Long id,String tierName,Double minScoreThreshold,String description,Boolean active){
+    this.id=id;
+    this.tierName=tierName;
+    this.minScoreThreshold=minScoreThreshold;
+    this.description=description;
+    this.active=active;
+}
+public UserEntity(){
+
+}
+public Long getId() {
+    return id;
+}
+public void setId(Long id) {
+    this.id = id;
+}
+public String getTierName() {
+    return tierName;
+}
+public void setTierName(String tierName) {
+    this.tierName = tierName;
+}
+public Double getMinScoreThreshold() {
+    return minScoreThreshold;
+}
+public void setMinScoreThreshold(Double minScoreThreshold) {
+    this.minScoreThreshold = minScoreThreshold;
+}
+public String getDescription() {
+    return description;
+}
+public void setDescription(String description) {
+    this.description = description;
+}
+public Boolean getActive() {
+    return active;
+}
+public void setActive(Boolean active) {
+    this.active = active;
+}
