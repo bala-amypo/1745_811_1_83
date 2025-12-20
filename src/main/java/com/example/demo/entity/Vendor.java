24f7fd3bb3982992d2b66519d8d087629fc11b15
bala-amypo <bala@amypo.in>
2025-12-20 17:31:09 +0000

    User commit

--- src/main/java/com/example/demo/entity/Vendor.java
+++ src/main/java/com/example/demo/entity/Vendor.java
@@ -11,31 +11,24 @@ public class Vendor {
     private Long id;
 
     private String name;
-    private String email;
+    private String contactEmail;
+    private String contactPhone;
+    private Boolean active = true;
 
     public Vendor() {}
 
-    public Long getId() {
-        return id;
-    }
+    public Long getId() { return id; }
+    public void setId(Long id) { this.id = id; }
 
-    public void setId(Long id) {
-        this.id = id;
-    }
+    public String getName() { return name; }
+    public void setName(String name) { this.name = name; }
 
-    public String getName() {
-        return name;
-    }
+    public String getContactEmail() { return contactEmail; }
+    public void setContactEmail(String contactEmail) { this.contactEmail = contactEmail; }
 
-    public void setName(String name) {
-        this.name = name;
-    }
+    public String getContactPhone() { return contactPhone; }
+    public void setContactPhone(String contactPhone) { this.contactPhone = contactPhone; }
 
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
+    public Boolean getActive() { return active; }
+    public void setActive(Boolean active) { this.active = active; }
 }
