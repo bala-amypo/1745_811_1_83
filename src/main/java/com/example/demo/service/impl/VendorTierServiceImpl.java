be142da067033dff7eddf3265184b1bc8e3358a1
bala-amypo <bala@amypo.in>
2025-12-21 10:44:44 +0000

    User commit

--- src/main/java/com/example/demo/service/impl/VendorTierServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/VendorTierServiceImpl.java
@@ -1,12 +1,14 @@
 package com.example.demo.service.impl;
 
-import com.example.demo.exception.ResourceNotFoundException;
 import com.example.demo.entity.VendorTier;
+import com.example.demo.exception.ResourceNotFoundException;
 import com.example.demo.repository.VendorTierRepository;
 import com.example.demo.service.VendorTierService;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Service
 public class VendorTierServiceImpl implements VendorTierService {
 
     private final VendorTierRepository repository;
