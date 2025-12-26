4a0e4d2ed73c34dd027700236b0ea87a980e87d9
bala-amypo <bala@amypo.in>
2025-12-26 18:23:47 +0000

    User commit

--- src/main/java/com/example/demo/service/impl/VendorTierServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/VendorTierServiceImpl.java
@@ -1,6 +1,6 @@
 package com.example.demo.service.impl;
 
-import com.example.demo.entity.VendorTier;
+import com.example.demo.model.VendorTier;
 import com.example.demo.exception.ResourceNotFoundException;
 import com.example.demo.repository.VendorTierRepository;
 import com.example.demo.service.VendorTierService;
