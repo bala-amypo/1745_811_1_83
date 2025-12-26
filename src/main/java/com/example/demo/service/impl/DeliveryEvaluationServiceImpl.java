8e159115f4695102fc5d93e633484d37a4b4715c
bala-amypo <bala@amypo.in>
2025-12-26 18:20:47 +0000

    User commit

--- src/main/java/com/example/demo/service/impl/DeliveryEvaluationServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/DeliveryEvaluationServiceImpl.java
@@ -2,7 +2,7 @@ package com.example.demo.service.impl;
 
 import com.example.demo.model.DeliveryEvaluation;
 import com.example.demo.model.SLARequirement;
-import com.example.demo.entiy.Vendor;
+import com.example.demo.model.Vendor;
 import com.example.demo.exception.ResourceNotFoundException;
 import com.example.demo.repository.DeliveryEvaluationRepository;
 import com.example.demo.repository.SLARequirementRepository;
--- src/main/java/com/example/demo/service/impl/SLARequirementServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/SLARequirementServiceImpl.java
@@ -1,6 +1,6 @@
 package com.example.demo.service.impl;
 
-import com.example.demo.entity.SLARequirement;
+import com.example.demo.model.SLARequirement;
 import com.example.demo.exception.ResourceNotFoundException;
 import com.example.demo.repository.SLARequirementRepository;
 import com.example.demo.service.SLARequirementService;
