cb72ef66b5a694a0260ab3df8672643fc7bdd711
bala-amypo <bala@amypo.in>
2025-12-27 05:08:15 +0000

    User commit

--- src/main/java/com/example/demo/service/DeliveryEvaluationService.java
+++ src/main/java/com/example/demo/service/DeliveryEvaluationService.java
@@ -7,8 +7,6 @@ public interface DeliveryEvaluationService {
 
     DeliveryEvaluation createEvaluation(DeliveryEvaluation evaluation);
 
-    DeliveryEvaluation getEvaluationById(Long id);
-
     List<DeliveryEvaluation> getEvaluationsForVendor(Long vendorId);
 
     List<DeliveryEvaluation> getEvaluationsForRequirement(Long requirementId);
