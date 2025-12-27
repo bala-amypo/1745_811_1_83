c87941a650bcf5722f71207983c0e2316a94cfbb
bala-amypo <bala@amypo.in>
2025-12-27 04:05:49 +0000

    User commit

--- src/main/java/com/example/demo/service/DeliveryEvaluationService.java
+++ src/main/java/com/example/demo/service/DeliveryEvaluationService.java
@@ -1,7 +1,6 @@
 package com.example.demo.service;
 
 import com.example.demo.model.DeliveryEvaluation;
-
 import java.util.List;
 
 public interface DeliveryEvaluationService {
--- src/main/java/com/example/demo/service/SLARequirementService.java
+++ src/main/java/com/example/demo/service/SLARequirementService.java
@@ -1,7 +1,6 @@
 package com.example.demo.service;
 
 import com.example.demo.model.SLARequirement;
-
 import java.util.List;
 
 public interface SLARequirementService {
