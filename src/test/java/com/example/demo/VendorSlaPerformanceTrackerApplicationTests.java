28563b169cda207f13847b1de2d146aa45240fc1
bala-amypo <bala@amypo.in>
2025-12-29 09:15:09 +0000

    User commit

---
+++ src/test/java/com/example/demo/VendorSlaPerformanceTrackerApplicationTests.java
@@ -0,0 +1,9 @@
+package com.example.demo;
+
+import org.testng.annotations.Listeners;
+
+@Listeners(TestResultListener.class)
+public class VendorSlaPerformanceTrackerApplicationTests {
+    // Optional: you can leave empty because all tests are modular
+    // Or add any high-level integration test here
+}
